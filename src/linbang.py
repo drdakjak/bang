7c2d271dd87606eb24176bf1113faa672642701a
drdakjak <drdakjak@users.noreply.github.com>
2019-02-14 10:33:53 +0100

    Update linbang.py

--- src/linbang.py
+++ src/linbang.py
@@ -151,7 +151,8 @@ class LogisticBang:
         # dtheta = reg * estimate + weight * (prediction - label) * values
 
         dtheta = weight * (label - prediction) * values
-        dtheta = np.clip(dtheta, a_min=1e-30, a_max=np.inf)
+        print(dtheta.min())
+#         dtheta = np.clip(dtheta, a_min=1e-30, a_max=np.inf)
         
         self.dtheta[ids] = dtheta
         self._prev_ids = ids
